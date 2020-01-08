ae487d9e12c4076c8947f4769b5953a50b753fe3
Bojo <bozhidar_zahariev@hotmail.com>
2020-01-08 09:52:12 +0000

    map

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+const Cat1 = props => {
+  return <div>1</div>;
+};
+
+const Cat2 = props => {
+  return <div>2</div>;
+};
+
 class Container extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      myArray: [1, 2, 3]
+    };
   }
 
   render() {
-    return <div className='main'>test</div>;
+    const ar = [<Cat1 />, <Cat2 />];
+    const itemsMapped = ar.map(element => (
+      <li className='red' key={ar.indexOf(element)}>
+        {element}
+      </li>
+    ));
+    return <div className='main'>{itemsMapped}</div>;
   }
 }
 
