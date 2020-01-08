50520e4420407cbc24441e7169cd5b0b68fc6ba5
Bojo <bozhidar_zahariev@hotmail.com>
2020-01-08 11:35:54 +0000

    btn

--- script.js
+++ script.js
@@ -6,14 +6,29 @@ const Cat2 = props => {
   return <div>2</div>;
 };
 
+const BtnM = props => {
+  return <button onClick={props.onClick}>map</button>;
+};
+
 class Container extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      myArray: [1, 2, 3]
+      mtd: ''
     };
+
+    //this.clickControl = this.clickControl.bind(this);
   }
+
+  /*
+  clickControl(par) {
+    this.setState({
+      mtd: par
+    });
+    console.log(this.state.mtd);
+  }
+  */
 
   render() {
     const ar = [<Cat1 />, <Cat2 />];
@@ -22,7 +37,12 @@ class Container extends React.Component {
         {element}
       </li>
     ));
-    return <div className='main'>{itemsMapped}</div>;
+    return (
+      <div className='main'>
+        <BtnM onClick={() => this.setState({ mtd: 'map' })} />
+        {this.state.mtd === 'map' ? itemsMapped : ar}
+      </div>
+    );
   }
 }
 
