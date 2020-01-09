77e2d0de2eea4e7c95ebf6997b53677b0f94fb59
Bojo <bozhidar_zahariev@hotmail.com>
2020-01-09 09:15:53 +0000

    classes

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ const Cat1 = props => {
 };
 
 const Cat2 = props => {
-  return <div>2</div>;
+  return <div className={props.className}>2</div>;
 };
 
 const Cats = props => {
   return (
     <div className={props.className}>
-      <Cat1 />
-      <Cat2 />
+      [
+      <Cat1 className={props.className} /> ,
+      <Cat2 className={props.className} />]
     </div>
   );
 };
 
-const BtnM = props => {
+const Btn = props => {
   return (
     <button className='btn' onClick={props.onClick}>
       {props.text}
@@ -38,6 +39,8 @@ class Container extends React.Component {
 
   render() {
     const ar = [<Cat1 />, <Cat2 />];
+
+    /*
     const itemsMapped = ar.map(element => (
       <div className='mapped' key={ar.indexOf(element)}>
         {element}
@@ -49,18 +52,23 @@ class Container extends React.Component {
         {element}
       </div>
     ));
-
+*/
     return (
       <div className='main'>
-        <BtnM text='map' onClick={() => this.setState({ mtd: 'map' })} />
-        <BtnM text='forEach' onClick={() => this.setState({ mtd: 'forEach' })} />
-        <BtnM text='filter' onClick={() => this.setState({ mtd: 'filter' })} />
-        {ar}
-        {this.state.mtd === 'map' ? <Cats className={'mapped'} /> : null}
-        {this.state.mtd === 'forEach' ? <Cats className={'forEach'} /> : null}
-        {this.state.mtd === 'filter' ? <Cat1 className={'filtered'} /> : null}
+        <div className='left'>
+          <Btn text='map' onClick={() => this.setState({ mtd: 'map' })} />
+          <Btn text='forEach' onClick={() => this.setState({ mtd: 'forEach' })} />
+          <Btn text='filter' onClick={() => this.setState({ mtd: 'filter' })} />
+        </div>
 
-        <BtnBck onClick={() => this.setState({ mtd: '' })} />
+        <div className='right'>
+          <Cats className={'init'} />
+          {this.state.mtd === 'map' ? <Cats className={'mapped'} /> : null}
+          {this.state.mtd === 'forEach' ? <Cats className={'forEach'} /> : null}
+          {this.state.mtd === 'filter' ? <Cat1 className={'filtered'} /> : null}
+
+          <BtnBck onClick={() => this.setState({ mtd: '' })} />
+        </div>
       </div>
     );
   }
