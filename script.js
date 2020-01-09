596494ce66368b1ffcfb54c120f6637265324289
Bojo <bozhidar_zahariev@hotmail.com>
2020-01-09 07:40:31 +0000

    more arrays

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const Cat1 = props => {
-  return <div>1</div>;
+  return <div className={props.className}>1</div>;
 };
 
 const Cat2 = props => {
   return <div>2</div>;
 };
 
+const Cats = props => {
+  return (
+    <div className={props.className}>
+      <Cat1 />
+      <Cat2 />
+    </div>
+  );
+};
+
 const BtnM = props => {
-  return <button onClick={props.onClick}>map</button>;
+  return (
+    <button className='btn' onClick={props.onClick}>
+      {props.text}
+    </button>
+  );
 };
 
 const BtnBck = props => {
@@ -21,31 +34,31 @@ class Container extends React.Component {
     this.state = {
       mtd: ''
     };
-
-    //this.clickControl = this.clickControl.bind(this);
   }
-
-  /*
-  clickControl(par) {
-    this.setState({
-      mtd: par
-    });
-    console.log(this.state.mtd);
-  }
-  */
 
   render() {
     const ar = [<Cat1 />, <Cat2 />];
     const itemsMapped = ar.map(element => (
-      <div className='red' key={ar.indexOf(element)}>
+      <div className='mapped' key={ar.indexOf(element)}>
         {element}
       </div>
     ));
+
+    const itemsForEach = ar.map(element => (
+      <div className='forEach' key={ar.indexOf(element)}>
+        {element}
+      </div>
+    ));
+
     return (
       <div className='main'>
-        <BtnM onClick={() => this.setState({ mtd: 'map' })} />
+        <BtnM text='map' onClick={() => this.setState({ mtd: 'map' })} />
+        <BtnM text='forEach' onClick={() => this.setState({ mtd: 'forEach' })} />
+        <BtnM text='filter' onClick={() => this.setState({ mtd: 'filter' })} />
         {ar}
-        {this.state.mtd === 'map' ? itemsMapped : null}
+        {this.state.mtd === 'map' ? <Cats className={'mapped'} /> : null}
+        {this.state.mtd === 'forEach' ? <Cats className={'forEach'} /> : null}
+        {this.state.mtd === 'filter' ? <Cat1 className={'filtered'} /> : null}
 
         <BtnBck onClick={() => this.setState({ mtd: '' })} />
       </div>
